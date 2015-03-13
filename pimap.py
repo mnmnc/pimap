0cb98c0dc5a5c5f0d603012517d89e18756dbd92
mnmnc <chooseright+git@gmail.com>
2015-03-13 17:09:27 +0100

    WIP. 0.4

--- pimap.py
+++ pimap.py
@@ -3,13 +3,18 @@ import imaplib
 import email
 import getpass
 import re
+import sys
+import quopri
+
 
 class Mail():
 
 	def __init__(self, debug=False):
+
 		self.server_configurations = {}
 		self.email_credentials = {}
 		self.mailboxes_names_to_check = {}
+		self.emails = []
 
 
 		# Will hold connection to server
@@ -19,6 +24,48 @@ class Mail():
 
 		# debug
 		self.debug = debug
+
+	def print_notification(self, type, length, title, details):
+		notification_types = ['ERROR', 'INFO', 'WARNING', 'SUCCESS', 'CHOICE', 'SUB', 'INPUT']
+		title_length = length
+		details_length = 50
+		if type.upper() in notification_types:
+			if type.upper() == 'ERROR':
+				print('[ ERR ]',
+				      adjust_string(title, title_length),
+				      adjust_string(details, details_length)
+				)
+			elif type.upper() == 'INFO':
+				print('[ NFO ]',
+				      adjust_string(title, title_length),
+				      adjust_string(details, details_length)
+				)
+			elif type.upper() == 'WARNING':
+				print('[ WRG ]',
+				      adjust_string(title, title_length),
+				      adjust_string(details, details_length)
+				)
+			elif type.upper() == 'SUCCESS':
+				print('[ SUC ]',
+				      adjust_string(title, title_length),
+				      adjust_string(details, details_length)
+				)
+			elif type.upper() == 'CHOICE':
+				print('[ CHC ]',
+				      adjust_string(title, title_length),
+				      adjust_string(details, details_length)
+				)
+			elif type.upper() == 'SUB':
+				print('       ',
+				      adjust_string(title, title_length),
+				      adjust_string(details, details_length)
+				)
+			elif type.upper() == 'INPUT':
+				print('[ INP ]',
+				      adjust_string(title, title_length),
+				      adjust_string(details, 0),
+				      end=': '
+				)
 
 	def add_server_config(self, domain, server_address, server_port=993):
 		"""
@@ -30,8 +77,10 @@ class Mail():
 		"""
 		try:
 			self.server_configurations.update({domain: [server_address, server_port] })
+			if self.debug:
+				self.print_notification('success', 35,'Server configuration added:', domain)
 		except:
-			print("[ e ] Adding server configuration failed for:", domain, server_address, server_port)
+			self.print_notification('error', 35, 'Failed to add configuration for', domain)
 
 	def show_server_config(self, domain="ALL"):
 		"""
@@ -40,14 +89,25 @@ class Mail():
 		:param domain: ex.: gmail.com
 		:return:
 		"""
+
 		if domain == "ALL":
+			self.print_notification('info', 35, 'Available configurations:','')
 			for server_domain in self.server_configurations.keys():
-				print(server_domain, '\t', self.server_configurations[server_domain][0] +':'+ str(self.server_configurations[server_domain][1]))
+				self.print_notification('sub', 35, server_domain,
+				                        self.server_configurations[server_domain][0]
+				                        +':'+
+				                        str(self.server_configurations[server_domain][1])
+				)
+			print()
 		else:
 			if domain in self.server_configurations.keys():
-				print(domain, '\t', self.server_configurations[domain][0] +':'+ str(self.server_configurations[domain][1]))
+				self.print_notification('info', 35, 'Configuration for ' + domain + ':',
+				                        self.server_configurations[domain][0]
+				                        +':'+
+				                        str(self.server_configurations[domain][1]))
+
 			else:
-				print("[ e ] No configuration for domain:", domain)
+				self.print_notification('error', 35, 'Configuration unavailable for:', domain)
 
 	def add_account_credentials(self, domain, username, password='PromptMe', mailbox_name='Inbox'):
 		"""
@@ -59,14 +119,16 @@ class Mail():
 		"""
 		try:
 			if password == 'PromptMe':
-				password = getpass.getpass('Password for '+ username + '@' + domain + ': ')
+				self.print_notification('input', 35, 'Password for '+ username + '@' + domain, '')
+				sys.stdout.flush()
+				password = getpass.getpass('')
 
 				if domain in self.email_credentials:
 					self.email_credentials[domain].update({username : [password, mailbox_name]})
 				else:
 					self.email_credentials.update({domain : {username : [password, mailbox_name]}})
 		except:
-			print("[ e ] Adding credential configuration failed for:", domain, username)
+			self.print_notification('error', 35, 'Failed to add credentails for:', username + '@' + domain)
 
 	def show_account_credentials(self, domain='ALL'):
 		"""
@@ -76,19 +138,28 @@ class Mail():
 		:return:
 		"""
 		if domain == "ALL":
+			self.print_notification('info', 35, 'Configured credentials:', '')
 			for server_domain in self.email_credentials.keys():
-				print(server_domain)
-
 				for user in self.email_credentials[server_domain].keys():
-					print('\t', user +':'+ self.email_credentials[server_domain][user][0], '\tBOX:', self.email_credentials[server_domain][user][1] )
+					self.print_notification('sub', 35,
+					                        user + '@' + server_domain + ' / ' +
+					                        self.email_credentials[server_domain][user][1],
+					                        self.email_credentials[server_domain][user][0]
+					                        )
+			print()
 
 		else:
 			if domain in self.email_credentials.keys():
-				print(domain)
+				self.print_notification('info', 35, 'Configured credentials:', '')
 				for user in self.email_credentials[domain].keys():
-					print('\t', user +':'+ self.email_credentials[domain][user][0], '\tBOX:', self.email_credentials[domain][user][1] )
+					self.print_notification('sub', 35,
+					                        user + '@' + domain + ' / ' +
+					                        self.email_credentials[domain][user][1],
+					                        self.email_credentials[domain][user][0]
+					                        )
+				print()
 			else:
-				print("[ e ] No configuration for domain:", domain)
+				self.print_notification('error', 35, 'No configuration available for:', domain)
 
 	def connect(self, domain):
 		"""
@@ -106,9 +177,10 @@ class Mail():
 				self.con = imaplib.IMAP4_SSL(self.server_configurations[domain][0], self.server_configurations[domain][1])
 				self.connected_domain = domain
 			except:
-				print("[ e ] Unable to establish IMAP4_SSL connection.", )
+				self.print_notification('error', 35, 'IMAP4_SSL connection failure:', domain)
+
 		else:
-			print("[ e ] Unable to establish connection. No configuration for domain:", domain)
+			self.print_notification('error', 35, 'No configuration for domain:', domain)
 
 	def login(self, domain, user):
 		"""
@@ -128,16 +200,19 @@ class Mail():
 						result = self.con.login(user + '@' + domain, self.email_credentials[domain][user][0])
 					except Exception as err:
 						error = str(err.args[0].decode('utf-8'))
-						print('[ e ] Login unsuccessful.', error)
+						self.print_notification('error', 35, 'Login unsuccessful:', error)
+				else:
+					self.print_notification('error', 35, 'No credentials for:', user + '@' + domain)
 			else:
-				print("[ e ] Unable to login. Connected to domain:", self.connected_domain, ". Login attempt to domain:", domain)
+				self.print_notification('error', 35, 'No credentials for:', domain)
+		else:
+			self.print_notification('error', 35, 'Domain mismatch:', domain + '!=' + self.connected_domain)
 
 		if result is not None and len(result) > 0:
 			if result[0] == "OK":
-				print('Logon successful.')
+				self.print_notification('success', 35, 'Logon successful:', user + '@' + domain)
 				self.connected_user = user
 				return 0
-
 		return -1
 
 	def logoff(self):
@@ -145,14 +220,14 @@ class Mail():
 		Logs off from current mailbox connection.
 		:return:
 		"""
-		self.con.logout()
+		self.print_notification('info', 35, 'Logoff:', self.con.logout()[0])
 
 	def close(self):
 		"""
 		Closes currently opened mailbox.
 		:return:
 		"""
-		self.con.close()
+		self.print_notification('info', 35, 'Closing connection:', self.con.close()[0])
 
 	def check_mailbox_stats(self):
 		"""
@@ -164,26 +239,28 @@ class Mail():
 		domain = self.connected_domain
 		user = self.connected_user
 
+
 		try:
 			# Get info
+			mailbox = self.email_credentials[domain][user][1]
 			check, stats = self.con.status(self.email_credentials[domain][user][1], '(MESSAGES UNSEEN)')
 
 			if check == 'OK':
 				# Cleanse info
-				stats_data = list((stats[0]).decode('latin-1').replace(self.email_credentials[domain][user][1],'').strip('"').strip().strip(')').split(' '))
+				stats_data = list((stats[0]).decode('latin-1').replace(mailbox,'').strip('"').strip().strip(')').split(' '))
+				all_messages = stats_data[1]
+				unseen_messages = stats_data[3]
 
 				# Print notification
-				print( adjust_string(user +'@'+ domain, 30),
-				       self.email_credentials[domain][user][1],
-				       stats_data[3] +'/'+ stats_data[1])
+				self.print_notification('info', 35, user +'@'+ domain + ' / ' + mailbox, unseen_messages +'/'+ all_messages)
+
 			else:
 				# Notify about failure
-				print('[ e ] Mailbox',
-				      self.email_credentials[self.connected_domain][self.connected_user][1],
-				      'is not available.' )
+				self.print_notification('error', 35, 'Mailbox unavailable:', user +'@'+ domain + ' / ' + mailbox)
 
-		except:
-			print('[ e ] Mailbox check failed.')
+		except Exception as err:
+			self.print_notification('error', 35, 'Mailbox status check failed:',err.args[0])
+
 
 	def check_message_details(self, id):
 
@@ -214,11 +291,93 @@ class Mail():
 		"""
 
 		# TODO: extend capabilities
+		self.print_notification('info', 35, 'Checking message inner details:','')
+		self.check_message_inner_details(raw_email)
 
-		email_message = email.message_from_string(raw_email)
-		print('\tTo:', email_message['To'])
-		print('\tFr:', email_message['From'])
-		print('\tSb:', email_message['Subject'])
+		#email_message = email.message_from_string(raw_email.decode('latin-1'))
+
+
+		#self.print_message(email_message, ['To', 'From', 'Received', 'Subject', 'Date'])
+		# print(email_message.keys())
+		# print('\tTo:', email_message['To'])
+		# print('\tFr:', email_message['From'])
+		# print('\tSb:', email_message['Subject'])
+
+	def parse_header(self, header):
+		print('quopri:',quopri.decodestring(header))
+		(text, encoding) = email.header.decode_header(header)[0]
+		print(text)
+		print(encoding)
+
+	def check_message_inner_details(self, raw_email):
+
+		email_message_latin = ''
+		email_message_utf8 = ''
+		email_message_iso88591 = ''
+		email_message_iso88592 = ''
+		print('Begin')
+
+		try:
+			email_message_latin = email.message_from_string(raw_email.decode('latin-1'))
+
+			print(email_message_latin['To'])
+
+		except:
+			print('E latin')
+			pass
+
+		try:
+			email_message_utf8 = email.message_from_string(raw_email.decode('utf-8'))
+			self.parse_header(email_message_utf8['Subject'])
+			print(email_message_utf8['Subject'])
+		except:
+			print('E utf')
+			pass
+
+		try:
+			email_message_iso88591 = email.message_from_string(raw_email.decode('iso-8859-1'))
+			print(email_message_iso88591['To'])
+		except:
+			print('E 88591')
+			pass
+
+		try:
+			email_message_iso88592 = email.message_from_string(raw_email.decode('iso-8859-2'))
+			print(email_message_iso88592['To'])
+		except:
+			print('E 88592')
+			pass
+
+		if email_message_latin == '':
+			print('latin failed')
+
+		if email_message_utf8 == '':
+			print('utf8 failed')
+		if email_message_iso88591 == '':
+			print('iso88591 failed')
+		if email_message_iso88592 == '':
+			print('iso88592 failed')
+
+		print('End')
+
+
+	def print_message(self, email_message, fields, verbosity='0'):
+		print(email_message)
+
+		if verbosity == '0':
+			for field in fields:
+				if field in email_message.keys():
+					values = email_message.get_all(field)
+					for value in values:
+						self.print_notification('sub', 35, field, value)
+
+		elif verbosity == '1':
+			pass
+		elif verbosity.upper() == 'multiline':
+			pass
+		else:
+			pass
+
 
 	def set_msg_as_seen(self, id):
 		"""
@@ -239,6 +398,7 @@ class Mail():
 		"""
 		self.con.select(self.email_credentials[self.connected_domain][self.connected_user][1], readonly=False)
 		result, data = self.con.uid('search', None, scope)
+		print(data)
 		if result == 'OK':
 			if len(data) > 0:
 				return data[0].split()
@@ -255,14 +415,20 @@ class Mail():
 		if result == 'OK':
 			#print(data)
 			raw_email = data[0][1]
-			raw_email = raw_email.decode('utf-8')
 			self.set_msg_as_unseen(uid)
+			self.print_notification('success', 35, 'Email fetched:', str(uid))
 		else:
-			print('Fetching mail failed.',result)
+			self.print_notification('error', 35, 'Fetching email failed for:', str(uid))
 		return raw_email
 
+	def get_message_peek_by_uid(self, uid):
 
+		raw_email = ''
+		result, data = self.con.uid('fetch', uid, '(BODY.PEEK[HEADER])')
+		if result == 'OK':
 
+			email_message = email.message_from_string(data[0][1].decode('latin-1'))
+			print(email_message)
 
 def main():
 
@@ -274,54 +440,53 @@ def main():
 
 	# Adding credentials
 	mail.add_account_credentials('gmail.com', '')
+	#mail.add_account_credentials('gmail.com', '')
 	# mail.add_account_credentials('gmail.com', '')
 	# mail.add_account_credentials('gmail.com', '')
-	# mail.add_account_credentials('gmail.com', '')
-	mail.add_account_credentials('outlook.com', '')
 	# mail.add_account_credentials('outlook.com', '')
+	#mail.add_account_credentials('outlook.com', '')
 	# mail.add_account_credentials('outlook.com', '')
 
 	# Showing configurations
-	if mail.debug:
-		mail.show_server_config()
-		mail.show_account_credentials()
+	# if mail.debug:
+	# 	mail.show_server_config()
+	# 	mail.show_server_config('test.com')
+	# 	mail.show_server_config('outlook.com')
+	# 	mail.show_account_credentials()
+	# 	mail.show_account_credentials('outlook.com')
+	# 	mail.show_account_credentials('sdsad.com')
 
 	# Login to mailbox
-	mail.login('gmail.com', '')
-	mail.check_mailbox_stats()
+
 	#mail.check_message_unseen()
 
-	unseen = mail.get_uids('UNSEEN')
-
-	if len(unseen) > 0:
-		unseen_all = unseen[0].split()
-		print(unseen_all)
-		for uid in unseen_all:
-			msg = mail.get_message_by_uid(uid)
-			msg_parsed = mail.parse_message(msg)
-
-
-
-	# mail.login('gmail.com', '')
-	# mail.check_mailbox_stats()
-	#
-	# mail.login('gmail.com', '')
-	# mail.check_mailbox_stats()
-	#
-	# mail.login('gmail.com', '')
-	# mail.check_mailbox_stats()
-	#
-	mail.login('outlook.com', '')
+	mail.login('gmail.com', '')
 	mail.check_mailbox_stats()
 
-	unseen = mail.get_uids()
+	unseen = mail.get_uids('ALL')
 
 	if len(unseen) > 0:
-		unseen_all = unseen[0].split()
-		print(unseen_all)
-		for uid in unseen_all:
-			msg = mail.get_message_by_uid(uid)
-			msg_parsed = mail.parse_message(msg)
+
+		for uid in unseen:
+			#msg = mail.get_message_by_uid(uid)
+			msg = mail.get_message_peek_by_uid(uid)
+
+			#msg_parsed = mail.parse_message(msg)
+
+
+
+	# mail.login('gmail.com', '')
+	# mail.check_mailbox_stats()
+	#
+	# mail.login('gmail.com', '')
+	# mail.check_mailbox_stats()
+	#
+	# mail.login('gmail.com', '')
+	# mail.check_mailbox_stats()
+	#
+	# mail.login('outlook.com', '')
+	# mail.check_mailbox_stats()
+	#
 
 	# mail.login('outlook.com', '')
 	# mail.check_mailbox_stats()
